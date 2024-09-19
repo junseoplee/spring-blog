fe929051dc254f0644167873055807e3f0f27608
junseoplee <junseoplee@soongsil.ac.kr>
2024-09-19 15:49:13 +0900

    feat: 관련 ErrorCode 추가 (#5)

--- src/main/java/project/springblog/exception/ErrorCode.java
+++ src/main/java/project/springblog/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ErrorCode {
 
-  DUPLICATE_MAIL(HttpStatus.BAD_REQUEST, "[Error] 중복된 이메일입니다.");
+  DUPLICATE_MAIL(HttpStatus.BAD_REQUEST, "[Error] 중복된 이메일입니다."),
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "[Error] 사용자를 찾을 수 없습니다."),
+  WRONG_PASSWORD(HttpStatus.BAD_REQUEST, "[Error] 잘못된 비밀번호입니다.");
 
   private final HttpStatus status;
   private final String message;
