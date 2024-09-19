e16ca8439ec0384f106a62174d87b7645dbed50f
junseoplee <junseoplee@soongsil.ac.kr>
2024-09-19 20:40:13 +0900

    feat: 댓글 수정 기능 구현 (#11)

--- src/main/java/project/springblog/api/comment/CommentController.java
+++ src/main/java/project/springblog/api/comment/CommentController.java
@@ -4,14 +4,17 @@ import jakarta.validation.Valid;
 import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import project.springblog.api.comment.request.CommentCreateRequest;
+import project.springblog.api.comment.request.CommentUpdateRequest;
 import project.springblog.application.comment.CommentService;
 import project.springblog.application.comment.response.CommentCreateResponse;
+import project.springblog.application.comment.response.CommentUpdateResponse;
 
 @RestController
 @RequestMapping("/api/comments")
@@ -28,5 +31,15 @@ public class CommentController {
     CommentCreateResponse response = commentService.createComment(articleId, request.toServiceRequest());
     URI location = URI.create(COMMENT_URI + response.getCommentId());
     return ResponseEntity.created(location).body(response);
+  }
+
+  @PatchMapping("/{articleId}/{commentId}")
+  public ResponseEntity<CommentUpdateResponse> updateComment(
+      @PathVariable Long articleId,
+      @PathVariable Long commentId,
+      @Valid @RequestBody CommentUpdateRequest request
+  ) {
+    CommentUpdateResponse response = commentService.updateComment(articleId, commentId, request.toServiceRequest());
+    return ResponseEntity.ok(response);
   }
 }
--- src/main/java/project/springblog/application/comment/CommentService.java
+++ src/main/java/project/springblog/application/comment/CommentService.java
@@ -4,7 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import project.springblog.application.comment.request.CommentCreateServiceRequest;
+import project.springblog.application.comment.request.CommentUpdateServiceRequest;
 import project.springblog.application.comment.response.CommentCreateResponse;
+import project.springblog.application.comment.response.CommentUpdateResponse;
 import project.springblog.application.validator.UserValidator;
 import project.springblog.domain.article.Article;
 import project.springblog.domain.article.repository.ArticleRepository;
@@ -39,5 +41,28 @@ public class CommentService {
     Comment savedComment = commentRepository.save(comment);
 
     return new CommentCreateResponse(savedComment.getId(), existUser.getEmail(), savedComment.getContent());
+  }
+
+  @Transactional
+  public CommentUpdateResponse updateComment(Long articleId, Long commentId, CommentUpdateServiceRequest request) {
+    User existUser = userValidator.validateUserAndPassword(request.getEmail(), request.getPassword());
+
+    Article existArticle = articleRepository.findById(articleId)
+                                            .orElseThrow(() -> new BusinessException(ErrorCode.ARTICLE_NOT_FOUND));
+
+    Comment foundComment = commentRepository.findById(commentId)
+                                           .orElseThrow(() -> new BusinessException(ErrorCode.COMMENT_NOT_FOUND));
+
+    if (!foundComment.getUser().getEmail().equals(request.getEmail())) {
+      throw new BusinessException(ErrorCode.USER_NOT_MATCH);
+    }
+
+    if (!foundComment.getArticle().getId().equals(existArticle.getId())) {
+      throw new BusinessException(ErrorCode.ARTICLE_NOT_MATCH);
+    }
+
+    foundComment.update(request.getContent());
+
+    return new CommentUpdateResponse(foundComment.getId(), existUser.getEmail(), foundComment.getContent());
   }
 }
